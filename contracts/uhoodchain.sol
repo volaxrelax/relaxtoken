2590b04fdc4e9689b890351dbd42c83a1da5773f
Zeming Yu <zemingyu@gmail.com>
2018-05-27 21:50:55 +1000

    smart contract initial set up

--- contracts/uhoodchain.sol
+++ contracts/uhoodchain.sol
@@ -248,3 +248,224 @@ library Properties {
         return self.index.length;
     }
 }
+
+// ----------------------------------------------------------------------------
+// Uhood
+// ----------------------------------------------------------------------------
+contract Uhood {
+    using SafeMath for uint;
+    using Properties for Properties.Data;
+    // using Proposals for Proposals.Data;
+
+    // string public name;
+
+    UhoodTokenInterface public token;
+    Properties.Data Properties;
+    // Proposals.Data public proposals;
+    bool public initialised;
+
+    uint public tokensForNewProperties;
+
+    uint public quorum = 80;
+    uint public quorumDecayPerWeek = 10;
+    uint public requiredMajority = 70;
+
+
+    // Must be copied here to be added to the ABI
+    event MemberAdded(address indexed memberAddress, string name, uint totalAfter);
+    event MemberRemoved(address indexed memberAddress, string name, uint totalAfter);
+    event MemberNameUpdated(address indexed memberAddress, string oldName, string newName);
+
+    event NewProposal(uint indexed proposalId, Proposals.ProposalType indexed proposalType, address indexed proposer); 
+    event Voted(uint indexed proposalId, address indexed voter, bool vote, uint votedYes, uint votedNo);
+    event VoteResult(uint indexed proposalId, bool pass, uint votes, uint quorumPercent, uint PropertiesLength, uint yesPercent, uint requiredMajority);
+    event TokenUpdated(address indexed oldToken, address indexed newToken);
+    event TokensForNewPropertiesUpdated(uint oldTokens, uint newTokens);
+    event EtherDeposited(address indexed sender, uint amount);
+    event EtherTransferred(uint indexed proposalId, address indexed sender, address indexed recipient, uint amount);
+
+
+    modifier onlyMember {
+        require(Properties.isMember(msg.sender));
+        _;
+    }
+
+    constructor(string clubName, address clubEthToken, uint _tokensForNewProperties) public {
+        Properties.init();
+        name = clubName;
+        token = ClubEthTokenInterface(clubEthToken);
+        tokensForNewProperties = _tokensForNewProperties;
+    }
+    function init(address memberAddress, string memberName) public {
+        require(!initialised);
+        initialised = true;
+        Properties.add(memberAddress, memberName);
+        token.mint(memberAddress, tokensForNewProperties);
+    }
+    function setMemberName(string memberName) public {
+        Properties.setName(msg.sender, memberName);
+    }
+    function proposeAddMember(string memberName, address memberAddress) public onlyMember returns (uint proposalId) {
+        proposalId = proposals.proposeAddMember(memberName, memberAddress);
+        vote(proposalId, true);
+    }
+    function proposeRemoveMember(string description, address memberAddress) public onlyMember returns (uint proposalId) {
+        proposalId = proposals.proposeRemoveMember(description, memberAddress);
+        vote(proposalId, true);
+    }
+    function proposeMintTokens(string description, address tokenOwner, uint amount) public onlyMember returns (uint proposalId) {
+        proposalId = proposals.proposeMintTokens(description, tokenOwner, amount);
+        vote(proposalId, true);
+    }
+    function proposeBurnTokens(string description, address tokenOwner, uint amount) public onlyMember returns (uint proposalId) {
+        proposalId = proposals.proposeBurnTokens(description, tokenOwner, amount);
+        vote(proposalId, true);
+    }
+    function proposeEtherTransfer(string description, address recipient, uint amount) public onlyMember returns (uint proposalId) {
+        proposalId = proposals.proposeEtherTransfer(description, recipient, amount);
+        vote(proposalId, true);
+    }
+    function voteNo(uint proposalId) public onlyMember {
+        vote(proposalId, false);
+    }
+    function voteYes(uint proposalId) public onlyMember {
+        vote(proposalId, true);
+    }
+    function vote(uint proposalId, bool yesNo) internal {
+        proposals.vote(proposalId, yesNo, Properties.length(), getQuorum(proposals.getInitiated(proposalId), now), requiredMajority);
+        Proposals.ProposalType proposalType = proposals.getProposalType(proposalId);
+        if (proposals.toExecute(proposalId)) {
+            string memory description = proposals.getDescription(proposalId);
+            address address1  = proposals.getAddress1(proposalId);
+            uint amount = proposals.getAmount(proposalId);
+            if (proposalType == Proposals.ProposalType.AddMember) {
+                Properties.add(address1, description);
+                token.mint(address1, tokensForNewProperties);
+            } else if (proposalType == Proposals.ProposalType.RemoveMember) {
+                Properties.remove(address1);
+                token.burn(address1, uint(-1));
+            } else if (proposalType == Proposals.ProposalType.MintTokens) {
+                token.mint(address1, amount);
+            } else if (proposalType == Proposals.ProposalType.BurnTokens) {
+                token.burn(address1, amount);
+            } else if (proposalType == Proposals.ProposalType.EtherTransfer) {
+                address1.transfer(amount);
+                emit EtherTransferred(proposalId, msg.sender, address1, amount);
+            }
+            proposals.close(proposalId);
+        }
+    }
+
+    function getVotingStatus(uint proposalId) public view returns (bool, bool, uint, uint){
+        return proposals.getVotingStatus(proposalId, Properties.length(), getQuorum(proposals.getInitiated(proposalId), now), requiredMajority);
+    }
+
+
+    /*
+    function setToken(address clubToken) internal {
+        emit TokenUpdated(address(token), clubToken);
+        token = ClubTokenInterface(clubToken);
+    }
+    function setTokensForNewProperties(uint _tokensForNewProperties) internal {
+        emit TokensForNewPropertiesUpdated(tokensForNewProperties, _tokensForNewProperties);
+        tokensForNewProperties = _tokensForNewProperties;
+    }
+    function addMember(address memberAddress, string memberName) internal {
+        Properties.add(memberAddress, memberName);
+        token.mint(memberAddress, tokensForNewProperties);
+    }
+    function removeMember(address memberAddress) internal {
+        Properties.remove(memberAddress);
+    }
+    */
+
+    function numberOfProperties() public view returns (uint) {
+        return Properties.length();
+    }
+    function getProperties() public view returns (address[]) {
+        return Properties.index;
+    }
+    function getMemberData(address memberAddress) public view returns (bool _exists, uint _index, string _name) {
+        Properties.Member memory member = Properties.entries[memberAddress];
+        return (member.exists, member.index, member.name);
+    }
+    function getMemberByIndex(uint _index) public view returns (address _member) {
+        return Properties.index[_index];
+    }
+
+    function getQuorum(uint proposalTime, uint currentTime) public view returns (uint) {
+        if (quorum > currentTime.sub(proposalTime).mul(quorumDecayPerWeek).div(1 weeks)) {
+            return quorum.sub(currentTime.sub(proposalTime).mul(quorumDecayPerWeek).div(1 weeks));
+        } else {
+            return 0;
+        }
+    }
+    function numberOfProposals() public view returns (uint) {
+        return proposals.length();
+    }
+    function getProposal(uint proposalId) public view returns (uint _proposalType, address _proposer, string _description, address _address1, address _address2, uint _amount, uint _votedNo, uint _votedYes, uint _initiated, uint _closed) {
+        Proposals.Proposal memory proposal = proposals.proposals[proposalId];
+        _proposalType = uint(proposal.proposalType);
+        _proposer = proposal.proposer;
+        _description = proposal.description;
+        _address1 = proposal.address1;
+        _address2 = proposal.address2;
+        _amount = proposal.amount;
+        _votedNo = proposal.votedNo;
+        _votedYes = proposal.votedYes;
+        _initiated = proposal.initiated;
+        _closed = proposal.closed;
+    }
+    function () public payable {
+        emit EtherDeposited(msg.sender, msg.value);
+    }
+}
+
+
+// ----------------------------------------------------------------------------
+// ClubEth Factory
+// ----------------------------------------------------------------------------
+contract ClubEthFactory is Owned {
+
+    mapping(address => bool) _verify;
+    ClubEth[] public deployedClubs;
+    ClubEthTokenInterface[] public deployedTokens;
+
+    event ClubEthListing(address indexed clubAddress, string clubEthName,
+        address indexed tokenAddress, string tokenSymbol, string tokenName, uint8 tokenDecimals,
+        address indexed memberName, uint tokensForNewProperties);
+
+    function verify(address addr) public view returns (bool valid) {
+        valid = _verify[addr];
+    }
+    function deployClubEthContract(
+        string clubName,
+        string tokenSymbol,
+        string tokenName,
+        uint8 tokenDecimals,
+        string memberName,
+        uint tokensForNewProperties
+    ) public returns (ClubEth club, ClubEthToken token) {
+        token = new ClubEthToken(tokenSymbol, tokenName, tokenDecimals);
+        _verify[address(token)] = true;
+        deployedTokens.push(token);
+        club = new ClubEth(clubName, address(token), tokensForNewProperties);
+        token.transferOwnershipImmediately(address(club));
+        club.init(msg.sender, memberName);
+        _verify[address(club)] = true;
+        deployedClubs.push(club);
+        emit ClubEthListing(address(club), clubName, address(token), tokenSymbol, tokenName, tokenDecimals, msg.sender, tokensForNewProperties);
+    }
+    function numberOfDeployedClubs() public view returns (uint) {
+        return deployedClubs.length;
+    }
+    function numberOfDeployedTokens() public view returns (uint) {
+        return deployedTokens.length;
+    }
+    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    }
+    function () public payable {
+        revert();
+    }
+}
